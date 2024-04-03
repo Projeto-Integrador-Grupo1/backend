2ae44464d14944c59d7f3efa57151a8ee4b2f375
Alan Alves <alan.alvess@outlook.com>
2024-04-03 12:46:56 -0300

    Implementando CRUD: Post, GetAll e GetById

---
+++ src/main/java/com/generation/zerone/repository/CategoriaRepository.java
@@ -0,0 +1,9 @@
+package com.generation.zerone.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.generation.zerone.model.Categoria;
+
+public interface CategoriaRepository extends JpaRepository<Categoria, Long> {
+
+}
